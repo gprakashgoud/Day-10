deff27fccdc61bdbb9f2f30bc73cc3512d4d0826
gprakashgoud <gprakashgoud611@gmail.com>
2025-09-09 21:55:14 +0530

    Control Statements

---
+++ OnetoNsum.java
@@ -0,0 +1,20 @@
+package com.codegnan.loopingstatements;
+
+import java.util.Scanner;
+
+public class OnetoNsum {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number : ");
+		int number = scanner.nextInt();
+        int sum=0;
+        for (int i=1; i<=number; i++) {
+        	sum+=i;
+        	
+        }
+        System.out.println(sum);
+        scanner.close();
+	}
+
+}
